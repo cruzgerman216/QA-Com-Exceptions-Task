a628ad19363119e943964c6bfd5f2a7d79635483
cruzgerman216 <cruzgerman216@yahoo.com>
2021-05-26 09:46:35 -0500

    GC: took out extensive code for user input

--- src/com/qa/java/Runner.java
+++ src/com/qa/java/Runner.java
@@ -1,25 +1,36 @@
 package com.qa.java;
 
+import java.util.Scanner;
+
 import com.qa.java.calculator.Calculator;
 
 public class Runner {
+	public static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		double a = 4;
-		double b = 0;
-
+		System.out.println("Type two numbers for Division");
+		int a = 0;
+		int b = 0;
 		try {
-			if(b == 0) {
-				throw new ArithmeticException("Cannot divide by 0"); 
-			}
-			Calculator.division(4, 0);
-			
+			System.out.println("What is the first number?");
+			a = Integer.parseInt(scanner.nextLine());
+			b = Integer.parseInt(scanner.nextLine());
 		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		try {
+			if (b == 0) {
+				throw new ArithmeticException("Cannot divide by 0");
+			}
+
+			System.out.println(Calculator.division(a, b));
+		} catch (Exception e) {
 			System.out.println(e);
 		} finally {
 			System.out.println("Finished assessment.");
 		}
-
+		scanner.close();
 	}
 
 }
